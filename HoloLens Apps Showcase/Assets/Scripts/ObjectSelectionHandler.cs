596bb6ed8eb604d687ae84176f94e1998e188129
Stefan <skoopman@move4mobile.com>
2017-05-09 09:35:16 +0200

    Start/stop train with airtap

--- HoloLens Apps Showcase/Assets/Scripts/ObjectSelectionHandler.cs
+++ HoloLens Apps Showcase/Assets/Scripts/ObjectSelectionHandler.cs
@@ -5,22 +5,38 @@ using HoloToolkit.Unity.InputModule;
 
 public class ObjectSelectionHandler : MonoBehaviour,IInputClickHandler {
 
+    private bool isDriving = false;
+
     private void Start()
     {
-        
     }
 
     private void Update()
     {
-        
+        if (isDriving)
+        {
+            gameObject.transform.Translate(0.050f, 0, 0);
+            findPath(gameObject);
+        }
     }
 
 
     public void OnInputClicked(InputClickedEventData eventData)
     {
         Debug.Log("Train tapped");
+        isDriving = !isDriving;
+       
+    }
+
+    void findPath(GameObject coupe)
+    {
+        var pos = coupe.transform.position;
+        if (pos.x > 4 || pos.x < -4)
+        {
+            coupe.transform.Rotate(0, 2f, 0);
+        }
     }
 
 
-	
+
 }
