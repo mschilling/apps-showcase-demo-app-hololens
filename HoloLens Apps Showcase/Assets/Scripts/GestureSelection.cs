d50fae29d670d0b988ccbc5e94d20ec0491229ae
Stefan <skoopman@move4mobile.com>
2017-05-17 09:17:55 +0200

    Ruben's fix of Spatial Mapping object placement

---
+++ HoloLens Apps Showcase/Assets/Scripts/GestureSelection.cs
@@ -0,0 +1,26 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GestureSelection : MonoBehaviour, IInputClickHandler {
+
+    public PlaySpaceManager playSpaceManager;
+    public InputManager input;
+    
+    // Use this for initialization
+    void Start () {
+        input.AddGlobalListener(gameObject);
+	}
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Debug.Log("Clicked!");
+
+        if (!playSpaceManager.finishedScanning())
+        {
+            playSpaceManager.finishScanning();
+        }
+    }
+}
