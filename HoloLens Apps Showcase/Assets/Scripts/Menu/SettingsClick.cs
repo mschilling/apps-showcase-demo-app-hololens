e76e90cf71de4e37598f3c1141b3c7e535908fdf
Stefan <skoopman@move4mobile.com>
2017-06-28 11:39:41 +0200

    Click handlers for menu

---
+++ HoloLens Apps Showcase/Assets/Scripts/Menu/SettingsClick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class SettingsClick : MonoBehaviour, IInputClickHandler
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Debug.Log("Settings tapped");
+        // Go on
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ HoloLens Apps Showcase/Assets/Scripts/Menu/StartClick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class StartClick : MonoBehaviour, IInputClickHandler
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Debug.Log("Start tapped");
+        // Go on
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
